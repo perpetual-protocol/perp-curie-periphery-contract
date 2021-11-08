7c580d2d21fcafc2d0d9116ae485033bc655463b
Vinta Chen <vinta.chen@gmail.com>
2021-11-08 17:01:54 +0800

    create DelegatableVault

---
+++ contracts/DelegatableVault.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+import { AddressUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import { SafeOwnable } from "@perp/lushan/contracts/base/SafeOwnable.sol";
+import { IClearingHouse } from "@perp/lushan/contracts/interface/IClearingHouse.sol";
+import { IVault } from "@perp/lushan/contracts/interface/IVault.sol";
+import { DelegatableVaultStorageV1 } from "./storage/DelegatableVaultStorage.sol";
+import {
+    SafeERC20Upgradeable,
+    IERC20Upgradeable
+} from "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
+
+contract DelegatableVault is SafeOwnable, DelegatableVaultStorageV1 {
+    using AddressUpgradeable for address;
+    using SafeMathUpgradeable for uint256;
+
+    //
+    // MODIFIER
+    //
+
+    modifier onlyFundOwner() {
+        // DV_OFO: only fund owner
+        require(msg.sender == _fundOwner, "DV_OFO");
+        _;
+    }
+
+    modifier onlyFundOwnerOrFundManager() {
+        // DV_OFOFM: only fund owner or fund manager
+        require(msg.sender == _fundOwner || msg.sender == _fundManager, "DV_OFOFM");
+        _;
+    }
+
+    //
+    // EXTERNAL NON-VIEW
+    //
+
+    function initialize(
+        address clearingHouseArg,
+        address fundOwnerArg,
+        address fundManagerArg
+    ) external initializer {
+        // DV_CHNC: ClearingHouse address is not contract
+        require(clearingHouseArg.isContract(), "DV_CHNC");
+        _clearingHouse = clearingHouseArg;
+
+        _fundOwner = fundOwnerArg;
+        _fundManager = fundManagerArg;
+    }
+
+    function setFundManager(address fundManagerArg) external onlyOwner {
+        _fundManager = fundManagerArg;
+    }
+
+    //
+    // only fund owner
+    //
+    function deposit(address token, uint256 amountX10_D) external onlyFundOwner {
+        IVault vault = IVault(IClearingHouse(_clearingHouse).getVault());
+        SafeERC20Upgradeable.safeTransferFrom(IERC20Upgradeable(token), msg.sender, address(this), amountX10_D);
+        SafeERC20Upgradeable.safeApprove(IERC20Upgradeable(token), address(vault), amountX10_D);
+        vault.deposit(token, amountX10_D);
+    }
+
+    function withdraw(address token, uint256 amountX10_D) external onlyFundOwner {
+        IVault vault = IVault(IClearingHouse(_clearingHouse).getVault());
+        vault.withdraw(token, amountX10_D);
+        SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(token), msg.sender, amountX10_D);
+    }
+
+    //
+    // fund owner and fund manager
+    //
+    function addLiquidity(IClearingHouse.AddLiquidityParams calldata params)
+        external
+        onlyFundOwnerOrFundManager
+        returns (IClearingHouse.AddLiquidityResponse memory)
+    {}
+
+    function removeLiquidity(IClearingHouse.RemoveLiquidityParams calldata params)
+        external
+        onlyFundOwnerOrFundManager
+        returns (IClearingHouse.RemoveLiquidityResponse memory response)
+    {}
+
+    function openPosition(IClearingHouse.OpenPositionParams memory params)
+        external
+        onlyFundOwnerOrFundManager
+        returns (uint256 deltaBase, uint256 deltaQuote)
+    {}
+
+    function closePosition(IClearingHouse.ClosePositionParams calldata params)
+        external
+        onlyFundOwnerOrFundManager
+        returns (uint256 deltaAvailableBase, uint256 deltaAvailableQuote)
+    {}
+
+    function cancelExcessOrders(
+        address maker,
+        address baseToken,
+        bytes32[] calldata orderIds
+    ) external onlyFundOwnerOrFundManager {}
+
+    function cancelAllExcessOrders(address maker, address baseToken) external onlyFundOwnerOrFundManager {}
+}
