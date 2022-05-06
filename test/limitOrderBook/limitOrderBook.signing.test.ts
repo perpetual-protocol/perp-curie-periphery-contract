a95d52405e0092176c5cade30d0380fe409e91b5
Vinta Chen <vinta.chen@gmail.com>
2022-05-06 16:46:30 +0800

    fix signing tests

--- test/limitOrderBook/limitOrderBook.signing.test.ts
+++ test/limitOrderBook/limitOrderBook.signing.test.ts
@@ -8,7 +8,7 @@ import { createLimitOrderFixture, LimitOrderFixture } from "./fixtures"
 import { getOrderTypes } from "./orderUtils"
 
 describe("LimitOrderBook signing", function () {
-    const [admin, trader] = waffle.provider.getWallets()
+    const [admin, trader, alice] = waffle.provider.getWallets()
     let fixture: LimitOrderFixture
     let limitOrderBook: TestLimitOrderBook
     let baseToken: BaseToken
@@ -16,13 +16,10 @@ describe("LimitOrderBook signing", function () {
     beforeEach(async () => {
         fixture = await loadFixture(createLimitOrderFixture())
         limitOrderBook = fixture.limitOrderBook
-
-        fixture = await loadFixture(createLimitOrderFixture())
-        limitOrderBook = fixture.limitOrderBook
         baseToken = fixture.baseToken
     })
 
-    it.only("get order hash", async () => {
+    it("get order hash", async () => {
         const domain = {
             name: fixture.EIP712Name,
             version: fixture.EIP712Version,
@@ -33,9 +30,9 @@ describe("LimitOrderBook signing", function () {
         // long 1 ETH (base) at $3000 with $3000 (quote)
         const limitOrder = {
             orderType: fixture.orderTypeLimitOrder,
-            salt: 123,
+            salt: 1,
             trader: trader.address,
-            baseToken: "0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB",
+            baseToken: baseToken.address,
             isBaseToQuote: false, // long: Q2B
             isExactInput: true, // exact input: quote
             amount: parseEther("3000").toString(), // quote amount: $3000
@@ -61,64 +58,70 @@ describe("LimitOrderBook signing", function () {
         expect(orderHashOffChain).to.be.eq(orderHashOnChain)
     })
 
-    // it("get order hashes with the same parameters but different salt", async () => {
-    //     // long 1 ETH (base) at $3000 with $3000 (quote)
-    //     const limitOrder1 = {
-    //         salt: 1,
-    //         trader: trader.address,
-    //         baseToken: "0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB",
-    //         isBaseToQuote: false, // long: Q2B
-    //         isExactInput: true, // exact input: quote
-    //         amount: parseEther("3000").toString(), // quote amount: $3000
-    //         oppositeAmountBound: parseEther("1").toString(), // base amount: 1 ETH
-    //         deadline: ethers.constants.MaxUint256.toString(), // no expiration date
-    //         reduceOnly: false,
-    //     }
+    it("get order hashes with the same parameters but different salt", async () => {
+        // long 1 ETH (base) at $3000 with $3000 (quote)
+        const limitOrder1 = {
+            orderType: fixture.orderTypeLimitOrder,
+            salt: 1,
+            trader: trader.address,
+            baseToken: baseToken.address,
+            isBaseToQuote: false, // long: Q2B
+            isExactInput: true, // exact input: quote
+            amount: parseEther("3000").toString(), // quote amount: $3000
+            oppositeAmountBound: parseEther("1").toString(), // base amount: 1 ETH
+            deadline: ethers.constants.MaxUint256.toString(), // no expiration date
+            reduceOnly: false,
+            roundIdWhenCreated: parseEther("0").toString(),
+            triggerPrice: parseEther("0").toString(),
+        }
 
-    //     const limitOrder2 = {
-    //         ...limitOrder1,
-    //         salt: 2,
-    //     }
+        const limitOrder2 = {
+            ...limitOrder1,
+            salt: 2,
+        }
 
-    //     const orderHash1 = await limitOrderBook.getOrderHash(limitOrder1)
-    //     const orderHash2 = await limitOrderBook.getOrderHash(limitOrder2)
+        const orderHash1 = await limitOrderBook.getOrderHash(limitOrder1)
+        const orderHash2 = await limitOrderBook.getOrderHash(limitOrder2)
 
-    //     expect(orderHash1).to.not.eq(orderHash2)
-    // })
+        expect(orderHash1).to.not.eq(orderHash2)
+    })
 
-    // it("sign limit order", async () => {
-    //     const domain = {
-    //         name: fixture.EIP712Name,
-    //         version: fixture.EIP712Version,
-    //         chainId: (await waffle.provider.getNetwork()).chainId,
-    //         verifyingContract: limitOrderBook.address,
-    //     }
+    it("sign limit order", async () => {
+        const domain = {
+            name: fixture.EIP712Name,
+            version: fixture.EIP712Version,
+            chainId: (await waffle.provider.getNetwork()).chainId,
+            verifyingContract: limitOrderBook.address,
+        }
 
-    //     // long 2 ETH (base) at $3000 with $6000 (quote)
-    //     const limitOrder = {
-    //         salt: 123,
-    //         trader: trader.address,
-    //         baseToken: "0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB",
-    //         isBaseToQuote: false, // long: Q2B
-    //         isExactInput: true, // exact input: quote
-    //         amount: parseEther("6000").toString(), // quote amount: $6000
-    //         oppositeAmountBound: parseEther("2").toString(), // base amount: 2 ETH
-    //         deadline: ethers.constants.MaxUint256.toString(), // no expiration date
-    //         reduceOnly: false,
-    //     }
+        // long 2 ETH (base) at $3000 with $6000 (quote)
+        const limitOrder = {
+            orderType: fixture.orderTypeLimitOrder,
+            salt: 123,
+            trader: trader.address,
+            baseToken: baseToken.address,
+            isBaseToQuote: false, // long: Q2B
+            isExactInput: true, // exact input: quote
+            amount: parseEther("6000").toString(), // quote amount: $6000
+            oppositeAmountBound: parseEther("2").toString(), // base amount: 2 ETH
+            deadline: ethers.constants.MaxUint256.toString(), // no expiration date
+            reduceOnly: false,
+            roundIdWhenCreated: parseEther("0").toString(),
+            triggerPrice: parseEther("0").toString(),
+        }
 
-    //     const types = getOrderTypes()
-    //     const typesWithoutDomain = {
-    //         LimitOrder: types.LimitOrder,
-    //     }
+        const types = getOrderTypes()
+        const typesWithoutDomain = {
+            LimitOrder: types.LimitOrder,
+        }
 
-    //     // sign limit order
-    //     const signature = await trader._signTypedData(domain, typesWithoutDomain, limitOrder)
-    //     const signer = await limitOrderBook.verifySigner(limitOrder, signature)
-    //     expect(trader.address).to.be.eq(signer)
+        // sign limit order
+        const signature = await trader._signTypedData(domain, typesWithoutDomain, limitOrder)
+        const signer = await limitOrderBook.verifySigner(limitOrder, signature)
+        expect(trader.address).to.be.eq(signer)
 
-    //     // force error, sign limit order by another trader
-    //     const badSignature = await bob._signTypedData(domain, typesWithoutDomain, limitOrder)
-    //     await expect(limitOrderBook.verifySigner(limitOrder, badSignature)).to.be.revertedWith("LOB_SINT")
-    // })
+        // force error, sign limit order by another trader
+        const badSignature = await alice._signTypedData(domain, typesWithoutDomain, limitOrder)
+        await expect(limitOrderBook.verifySigner(limitOrder, badSignature)).to.be.revertedWith("LOB_SINT")
+    })
 })
