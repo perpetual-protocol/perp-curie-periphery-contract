31202824fba49071566d2877376e02f28f506e3b
Opass Chang <th33ts@gmail.com>
2022-09-12 15:24:31 +0800

    add test for SetLimitOrderBook in LimitOrderRewardVault

--- test/foundry/LimitOrderRewardVaultTest.sol
+++ test/foundry/LimitOrderRewardVaultTest.sol
@@ -3,10 +3,13 @@ pragma solidity 0.7.6;
 import "../../contracts/test/TestERC20.sol";
 import "../../contracts/limitOrder/LimitOrderRewardVault.sol";
 import "forge-std/Test.sol";
+import "../../contracts/test/TestLimitOrderBook.sol";
 
 contract LimitOrderRewardVaultTest is Test {
     LimitOrderRewardVault limitOrderRewardVault;
+    address constant nonOwnerAddress = address(0x1234);
     event RewardTokenChanged(address rewardToken);
+    event LimitOrderBookChanged(address limitOrderBook);
 
     function setUp() public {
         TestERC20 rewardToken1 = new TestERC20();
@@ -39,9 +42,34 @@ contract LimitOrderRewardVaultTest is Test {
     }
 
     function testSetRewardToken_only_able_to_set_by_owner() public {
-        address nonOwner = address(0x1234);
         vm.expectRevert(bytes("SO_CNO"));
-        vm.prank(nonOwner);
+        vm.prank(nonOwnerAddress);
         limitOrderRewardVault.setRewardToken(address(0));
+    }
+
+    function testSetLimitOrderBook_able_to_set_limitOrderBook() public {
+        LimitOrderBook limitOrderBook = new TestLimitOrderBook();
+        limitOrderRewardVault.setLimitOrderBook(address(limitOrderBook));
+        assertEq(address(limitOrderBook), limitOrderRewardVault.limitOrderBook());
+    }
+
+    function testSetLimitOrderBook_should_emit_event() public {
+        LimitOrderBook limitOrderBook = new TestLimitOrderBook();
+        limitOrderRewardVault.setLimitOrderBook(address(limitOrderBook));
+
+        vm.expectEmit(false, false, false, true);
+        emit LimitOrderBookChanged(address(limitOrderBook));
+        limitOrderRewardVault.setLimitOrderBook(address(limitOrderBook));
+    }
+
+    function testSetLimitOrderBook_unable_to_set_non_contract_address() public {
+        vm.expectRevert(bytes("LOFV_LOBINC"));
+        limitOrderRewardVault.setLimitOrderBook(address(0));
+    }
+
+    function testSetLimitOrderBook_only_able_to_set_by_owner() public {
+        vm.expectRevert(bytes("SO_CNO"));
+        vm.prank(nonOwnerAddress);
+        limitOrderRewardVault.setLimitOrderBook(address(0));
     }
 }
