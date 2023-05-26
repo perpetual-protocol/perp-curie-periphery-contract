e454078255ebc643f215daa5fe4d97a612f7b42f
Dylan Yang <dogi9527dogi9527@gmail.com>
2023-05-26 13:08:00 +0200

    test: fix OtcMaker.getter

--- test/foundry/OtcMaker/OtcMaker.getter.t.sol
+++ test/foundry/OtcMaker/OtcMaker.getter.t.sol
@@ -5,21 +5,18 @@ import { OtcMakerSetup } from "./helper/OtcMakerSetup.sol";
 import { IAccountBalance } from "@perp/curie-contract/contracts/interface/IAccountBalance.sol";
 
 contract OtcMakerGetterTest is OtcMakerSetup {
-    function test_isMarginSufficient() public {
+    function test_isMarginSufficient() public prepareFundOwner(5) {
         assertTrue(otcMaker.isMarginSufficient() == true);
 
-        _topUpUsdc(otcMakerOwner, 5);
-        vm.startPrank(otcMakerOwner);
         usdc.approve(address(otcMaker), type(uint256).max);
         otcMaker.deposit(address(usdc), 5);
-        vm.stopPrank();
 
         assertTrue(otcMaker.isMarginSufficient() == true);
 
         vm.mockCall(
             address(perp.accountBalance()),
             abi.encodeWithSelector(IAccountBalance.getTotalAbsPositionValue.selector),
-            abi.encode(10 * 10 ** (18 - usdc.decimals()))
+            abi.encode(10 * 10**(18 - usdc.decimals()))
         ); // margin ratio = 5 / 10 = 50%, leverage = 2x
 
         assertTrue(otcMaker.isMarginSufficient() == true);
@@ -27,7 +24,7 @@ contract OtcMakerGetterTest is OtcMakerSetup {
         vm.mockCall(
             address(perp.accountBalance()),
             abi.encodeWithSelector(IAccountBalance.getTotalAbsPositionValue.selector),
-            abi.encode(11 * 10 ** (18 - usdc.decimals()))
+            abi.encode(11 * 10**(18 - usdc.decimals()))
         ); // margin ratio = 5 / 11 = 45.4545%, leverage = 2.2x
 
         assertTrue(otcMaker.isMarginSufficient() == false);
