6e044b8ebb950211082aab01a8c1f97cb0fb41d3
Dylan Yang <dogi9527dogi9527@gmail.com>
2023-05-30 13:31:05 +0200

    doc: reorganized IOtcMaker NatSpec

--- contracts/interface/IOtcMaker.sol
+++ contracts/interface/IOtcMaker.sol
@@ -19,7 +19,7 @@ interface IOtcMaker is IOtcMakerStruct, IOtcMakerEvent {
     /// @param limitOrderParams The parameters of the limit order.
     /// @param jitLiquidityParams The JIT liquidity parameters.
     /// @param signature The signature associated with the limit order.
-    /// @return response The response containing the exchanged position size and notional value.
+    /// @return The response includes the exchanged position size and notional on OTCMaker.
     function openPositionFor(
         ILimitOrderBook.LimitOrder calldata limitOrderParams,
         JitLiquidityParams calldata jitLiquidityParams,
@@ -28,10 +28,10 @@ interface IOtcMaker is IOtcMakerStruct, IOtcMakerEvent {
 
     /// @notice Opens a position in the clearing house using the provided parameters.
     /// @dev This function can only be called by the designated position manager.
+    /// @dev Delegates the call to the clearing house to open the position.
     /// @param params The parameters for opening the position.
     /// @return base The amount of baseToken the taker got or spent
     /// @return quote The amount of quoteToken the taker got or spent
-    /// @dev Delegates the call to the clearing house to open the position.
     function openPosition(IClearingHouse.OpenPositionParams calldata params)
         external
         returns (uint256 base, uint256 quote);
@@ -44,25 +44,25 @@ interface IOtcMaker is IOtcMakerStruct, IOtcMakerEvent {
 
     /// @notice Withdraws a specified amount of tokens from the vault and transfers them to the fund owner.
     /// @dev This function can only be called by the designated fund owner.
+    /// @dev Delegates the call to the vault to withdraw the tokens and transfers them to the fund owner.
     /// @param token The address of the token to withdraw.
     /// @param amount The amount of tokens to withdraw.
-    /// @dev Delegates the call to the vault to withdraw the tokens and transfers them to the fund owner.
     function withdraw(address token, uint256 amount) external;
 
     /// @notice Withdraws the entire balance of a specific token from the contract and transfers it to the fund owner.
     /// @dev This function can only be called by the designated fund owner.
-    /// @param token The address of the token to withdraw.
     /// @dev Retrieves the balance of the specified token and transfers the full amount to the fund owner.
+    /// @param token The address of the token to withdraw.
     function withdrawToken(address token) external;
 
     /// @notice Claims a specific week of rewards for a liquidity provider using Merkle proofs.
     /// @dev This function can only be called by the designated fund owner.
+    /// @dev Delegates the call to the MerkleRedeem contract to claim rewards for the liquidity provider.
     /// @param merkleRedeem The address of the MerkleRedeem contract.
     /// @param liquidityProvider The address of the liquidity provider.
     /// @param week The week number to claim rewards for.
     /// @param claimedBalance The claimed amount.
     /// @param _merkleProof The Merkle proofs for the verification.
-    /// @dev Delegates the call to the MerkleRedeem contract to claim rewards for the liquidity provider.
     function claimWeek(
         address merkleRedeem,
         address liquidityProvider,
